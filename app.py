9933087d688e49948a74767b692b06a568f9aecd
newideas99 <56652858+newideas99@users.noreply.github.com>
2023-12-18 21:43:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from dotenv import find_dotenv, load_dotenv
-
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import MessagesPlaceholder
 from langchain.memory import ConversationSummaryBufferMemory
-from langchain.chains.summarize import load_summarize_chain
 from langchain.schema import SystemMessage
-from custom_tools import CreateEmailDraftTool, GenerateEmailResponseTool, ReplyEmailTool, EscalateTool, ProspectResearchTool, CategoriseEmailTool
+from custom_tools import (
+    CreateEmailDraftTool, GenerateEmailResponseTool, 
+    ReplyEmailTool, EscalateTool, ProspectResearchTool, CategoriseEmailTool
+)
 
+app = Flask(__name__)
 load_dotenv()
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+
+# Initialize your LangChain agent
+llm = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
 
 system_message = SystemMessage(
     content="""
     You are an email inbox assistant of a Realtor named Jacob Ferrari, 
-    who deals with buy, sells, rental, and adminstrative emails, 
-    Your goal is to handle all the incoming emails by categorising them based on 
+    who deals with buy, sells, rental, and administrative emails, 
+    Your goal is to handle all the incoming emails by categorizing them based on 
     guideline and decide on next steps
     """
 )
@@ -31,7 +35,7 @@ tools = [
 ]
 
 agent_kwargs = {
-    "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    "extra_prompt_messages": [],
     "system_message": system_message,
 }
 memory = ConversationSummaryBufferMemory(
@@ -46,43 +50,20 @@ agent = initialize_agent(
     memory=memory,
 )
 
+@app.route('/process-email', methods=['POST'])
+def process_email():
+    data = request.get_json()
+    email_content = data.get('emailBody')
 
-test_email = """
-Hi Jacob,
+    if not email_content:
+        return jsonify({"error": "No email content provided"}), 400
 
- 
+    try:
+        # Process the email content with your agent
+        response = agent({"input": email_content})
+        return jsonify({"message": "Email processed successfully", "response": response}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-I am glad my documents look great. Further to my e-mail, I will be living on my own and I have no pets. Sorry, I just saw your e-mail this morning. What dates and or times are you free this week for a viewing?
-
- 
-
-Thank you so much!
-
- 
-
-Best regards,
-
- 
-
-John (Jay) Newport
-
-Vice President
-
-LMClark-Logo-Blue-50
-
-L.M. Clark Customs Broker Ltd.
-
-1804 Alstep Drive Suite 200
-
-Mississauga, On
-
-L5S 1W1
-
-Direct line: 289-548-5085
-
-Fax:905-673-7345
-
-www.lmclark.com
-"""
-
-agent({"input": test_email})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
